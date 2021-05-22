ec442537fd35159fde4ef79ca6deb738b8a4a2f4
rtjoshuas <84062351+rtjoshuas@users.noreply.github.com>
2021-05-22 23:06:49 +0900

    Update rhombusEx.java

--- Workbook/rhombusEx.java
+++ Workbook/rhombusEx.java
@@ -23,19 +23,19 @@ public class rhombusEx {
 			else {
 			spaceCount += 1;
 			starCount -= 2;
-			System.out.println();
 			}
-		//		for(int j = 0; j < spaceCount; j++) {
-		//		System.out.print(" ");
-		//	}
+			
+	//		for(int j = 0; j < spaceCount; j++) {
+	//		System.out.print(" ");
+	//	}
 			
 			
 		}
-		/*lineCount = 3;*/
-		/*spaceCount = 1;*/
-		/*starCount = 5;*/
+	/*	lineCount = 3;
+		spaceCount = 1;
+		starCount = 5;
 			
-		/*for(int i = 0; i < lineCount; i++) {
+			for(int i = 0; i < lineCount; i++) {
 			for(int j = 0; j < spaceCount; j++) {
 				System.out.print(" ");
 			}
