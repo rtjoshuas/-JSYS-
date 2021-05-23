1c71d525bdf94373876d5ae122afb441f8e70166
rtjoshuas <84062351+rtjoshuas@users.noreply.github.com>
2021-05-24 00:54:08 +0900

    Create AbcArraysAlgorithm.java

---
+++ Workbook/AbcArraysAlgorithm.java
@@ -0,0 +1,25 @@
+
+
+public static void main(String[] args) {
+
+	Scanner sc = new Scanner(System.in);
+	
+	int countLine = sc.nextInt();
+	int strLine = countLine;
+	
+	char[] abc = new char[26];
+	char ch = "A";	
+	
+	for(int i = 0; i < countLine; i++){
+		int s;
+
+	for(int j = 0; j < strLine; j++) {
+		System.out.print(abc[s]);
+		s++;
+	}
+		strLine -= 1;
+	}
+
+
+
+}
