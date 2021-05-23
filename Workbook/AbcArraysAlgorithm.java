c05ff493323f4c396afc8a7ebd0de85a3bccf13d
rtjoshuas <84062351+rtjoshuas@users.noreply.github.com>
2021-05-24 01:20:41 +0900

    Update AbcArraysAlgorithm.java

--- Workbook/AbcArraysAlgorithm.java
+++ Workbook/AbcArraysAlgorithm.java
@@ -1,4 +1,6 @@
+import java.util.Scanner;
 
+public class AbcArraysAlgorithm {
 
 public static void main(String[] args) {
 
@@ -6,20 +8,26 @@ public static void main(String[] args) {
 	
 	int countLine = sc.nextInt();
 	int strLine = countLine;
+	int s = 0;
 	
 	char[] abc = new char[26];
-	char ch = "A";	
+	char ch = 'A';	
+	
+	for(int i = 0; i < abc.length; i++, ch++) {
+		abc[i] = ch;
+	}
 	
 	for(int i = 0; i < countLine; i++){
-		int s;
-
-	for(int j = 0; j < strLine; j++) {
+	
+		for(int j = 0; j < strLine; j++, s++) {
 		System.out.print(abc[s]);
-		s++;
+		
 	}
+
 		strLine -= 1;
+		System.out.println();
 	}
-
-
+	
+	}
 
 }
